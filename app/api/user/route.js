f1dced375dd2a70a21a912e2dba658213be49542
Israel Santana <is.wsie@gmail.com>
2024-09-20 16:39:46 -0400

    I need to implement the logic to transfer the registered user's data from Registration to User. Right now my users collection is empty and new registrations are not saving in users.

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,1 +1,20 @@
 import { connectToDB } from "@/utils/database";
+import User from "@models/user";
+
+export const GET = async (request) => {
+  try {
+    await connectToDB();
+
+    const { searchParams } = new URL(request.url);
+    const username = searchParams.get("username");
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return new Response("User not found", { status: 404 });
+    }
+
+    return new Response(JSON.stringify(user), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch user!", { status: 500 });
+  }
+};
