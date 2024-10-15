538e826818284de04e26316d40be8fae9c98ac8b
Israel Santana <is.wsie@gmail.com>
2024-10-15 16:52:41 -0400

    Finished the UI of the Settings bar.

--- components/ChatSettings.jsx
+++ components/ChatSettings.jsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
-// import Link from "next/link";
+import { ThemeIcon, GitHubIcon, DeleteIcon, LogoutIcon } from "@utils/svgfuncs";
 
 const ChatSettings = () => {
   const menuItems = [
     // Array to avoid code repetition.
-    { src: "/assets/icons/bx-user.svg", alt: "Theme" },
-    { src: "/assets/icons/bx-user.svg", alt: "GitHub" },
-    { src: "/assets/icons/bx-user.svg", alt: "Delete" },
-    { src: "/assets/icons/bx-user.svg", alt: "Log Out" },
+    { Icon: ThemeIcon, alt: "Theme" },
+    { Icon: GitHubIcon, alt: "GitHub" },
+    { Icon: DeleteIcon, alt: "Delete" },
+    { Icon: LogoutIcon, alt: "Log Out" },
   ];
   return (
     <div className="relative bottom-16 min-h-screen flex items-center p-4">
       <div className="absolute left-1/4 rounded-lg shadow-lg flex">
-        <div className="border-2 rounded-l-lg p-2 flex flex-col items-center justify-between">
+        <div className="bg-sky-600 border-2 rounded-l-lg p-2 flex flex-col items-center justify-between">
           <div className="mb-8">
             <Image
               src="/assets/icons/favicon.png"
@@ -24,13 +24,7 @@ const ChatSettings = () => {
           <div className="mt-28 space-y-6">
             {menuItems.map((item, index) => (
               <div key={index} className="group cursor-pointer">
-                <Image
-                  src={item.src}
-                  width={32}
-                  height={32}
-                  alt={item.alt}
-                  className="transition-all duration-300 group-hover:scale-110 group-hover:opacity-80"
-                />
+                <item.Icon className="w-9 h-9 fill-slate-200 transition-all duration-300 group-hover:scale-110 group-hover:opacity-80" />
               </div>
             ))}
           </div>
