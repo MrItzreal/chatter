2e363e4484c34f297ccae1f4a5c517c4610801e6
Israel Santana <is.wsie@gmail.com>
2024-10-17 10:36:18 -0400

    Improved responsiveness for both components.

--- components/ChatList.jsx
+++ components/ChatList.jsx
@@ -17,7 +17,7 @@ const ChatList = () => {
 
   return (
     <div className="bg-sky-600 border-2 rounded-r-lg flex flex-col flex-grow p-3">
-      <div className="flex items-center gap-3">
+      <div className="flex flex-col sm:flex-row items-center sm:items-center gap-3">
         <Image
           src="/assets/images/placeholder.jpg"
           alt="user_image"
@@ -25,30 +25,30 @@ const ChatList = () => {
           height={60}
           className="rounded-full object-contain"
         />
-        <div className="flex flex-col font-bold flex-grow">
-          <h2 className="flex items-center text-white text-2xl font-bold">
+        <div className="flex flex-col font-bold flex-grow w-full sm:w-auto">
+          <h2 className="flex items-center justify-center sm:justify-normal text-white text-xl sm:text-2xl font-bold">
             Alucard Tepes
           </h2>
-          <div className="flex items-center">
+          <div className="flex items-center w-full">
             {isEditing ? (
               <input
                 type="text"
-                className="border-4 border-double rounded outline-none px-3 h-8 flex-grow"
+                className="relative sm:right-4 border sm:border-4 border-double rounded outline-none flex-grow placeholder:text-center text-xs sm:text-sm px-2 py-1 scale-90"
                 placeholder="Share an update..."
                 value={status}
                 onChange={handleStatusChange}
                 onBlur={toggleEdit}
               />
             ) : (
-              <p className="text-white text-sm">
+              <p className="flex-grow flex items-center justify-center sm:justify-normal text-white text-sm ">
                 {status || "What's on your mind?"}
               </p>
             )}
             <button
               onClick={toggleEdit}
-              className="ml-2 text-white hover:text-gray-200"
+              className="text-white hover:text-gray-200 flex-shrink-0"
             >
-              <EditPencil className="fill-white" />
+              <EditPencil className="fill-white w-5 h-5" />
             </button>
           </div>
         </div>
--- components/ChatSettings.jsx
+++ components/ChatSettings.jsx
@@ -11,8 +11,8 @@ const ChatSettings = () => {
     { Icon: LogoutIcon, alt: "Log Out" },
   ];
   return (
-    <div className="relative bottom-16 min-h-screen flex items-center p-4">
-      <div className="absolute left-1/4 rounded-lg shadow-lg flex">
+    <div className="relative min-h-screen flex items-center p-4">
+      <div className="absolute left-1/4 rounded-lg shadow-lg flex scale-110">
         <div className="bg-sky-600 border-2 rounded-l-lg p-2 flex flex-col items-center justify-between">
           <div className="mb-8">
             <Image
