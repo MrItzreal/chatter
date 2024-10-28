3a7c13751d4dda465f413072921b675adadd2abd
Israel Santana <is.wsie@gmail.com>
2024-10-28 11:45:31 -0400

    Commented this code because is not being used for now

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,27 +1,30 @@
-import { connectToDB } from "@/utils/database";
-import User from "@models/user";
+/* I created this route in case I need to fetch an user or all users.
+However, currently this route is not being used anywhere in the app.*/
 
-export const GET = async (request) => {
-  try {
-    await connectToDB();
-    const { searchParams } = new URL(request.url);
-    const username = searchParams.get("username");
+// import { connectToDB } from "@/utils/database";
+// import User from "@models/user";
 
-    let result;
-    if (username) {
-      // Fetch One User:
-      result = await User.findOne({ username });
-      if (!result) {
-        return new Response("User not found", { status: 404 });
-      }
-    } else {
-      // Fetch All Users:
-      result = await User.find({});
-    }
+// export const GET = async (request) => {
+//   try {
+//     await connectToDB();
+//     const { searchParams } = new URL(request.url);
+//     const username = searchParams.get("username");
 
-    return new Response(JSON.stringify(result), { status: 200 });
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    return new Response("Failed to fetch users", { status: 500 });
-  }
-};
+//     let result;
+//     if (username) {
+//       // Fetch One User:
+//       result = await User.findOne({ username });
+//       if (!result) {
+//         return new Response("User not found", { status: 404 });
+//       }
+//     } else {
+//       // Fetch All Users:
+//       result = await User.find({});
+//     }
+
+//     return new Response(JSON.stringify(result), { status: 200 });
+//   } catch (error) {
+//     console.error("Error fetching users:", error);
+//     return new Response("Failed to fetch users", { status: 500 });
+//   }
+// };
